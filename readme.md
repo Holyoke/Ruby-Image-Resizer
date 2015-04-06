e4567ba2802e7f31a0861d250a4f113b47725a24
Holyoke <kid.idioteque@gmail.com>
2015-04-06 16:38:10 -0700

    Clean up for display

--- readme.md
+++ readme.md
@@ -1,15 +1,16 @@
-
 ## Challenge
 
 Create a function that will scale the aspect ratio of image dimension ([w,h]) to fit inside of a 200x200 box by using scale to fit. Meaning that one side, the width or height, will be 200 and the other will maintain the aspect ratio.
 The function should be able to take in an array of multiple w,h pairs ([w,h,w,h,w,h,w,h]) and return an array of the scaled down ratios
 
-sample input:
-[200,200]
-[400,200]
+```
+sample input:  
+[200,200]  
+[400,200]  
 [1256,1200, 600, 800, 200, 200, 400, 200, 800,1256]
 
-sample output:
-[200,200]
-[200, 100]
-[200, 191, 150, 200, 200, 200, 200, 100, 127, 200]
+sample output:  
+[200,200]  
+[200, 100]  
+[200, 191, 150, 200, 200, 200, 200, 100, 127, 200]  
+```
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -2,7 +2,7 @@ require "rspec"
 require "main.rb"
 require "byebug"
 
-describe "#image_resize" do
+describe "#image_resizer" do
 
 	context "Analysis" do
 
@@ -98,34 +98,5 @@ describe "#image_resize" do
 			end
 		end
 
-
-	end
-
-	context "Sample Input" do
-		let(:sample_input) { File.read("spec/sample_input.txt") }
-		let(:sample_output) { File.read("spec/sample_output.txt")}
-
-		describe "Sample Input Tests" do
-			it "passes the first sample test" do 
-				input = sample_input.split("\n")[0]
-				output = sample_output.split("\n")[0].to_i
-
-				# result = multiples_of_a_number(input)
-
-
-				expect(result).to eq(output)
-			end
-
-			it "passes the second sample test" do 
-				input = sample_input.split("\n")[1]
-				output = sample_output.split("\n")[1].to_i
-
-				# result = multiples_of_a_number(input)
-
-
-				expect(result).to eq(output)
-			end
-
-		end
 	end
 end
