37f5ce9b28e9a9a52c23ad47ad90790c2c5cc036
Holyoke <kid.idioteque@gmail.com>
2015-04-06 16:53:56 -0700

    Clean up for display

--- lib/main.rb
+++ lib/main.rb
@@ -4,46 +4,46 @@ require "set"
 # test = File.read("spec/test_input.txt").split("\n")
 
 def image_resizer(input)
-	dimensions = parse(input)
-	result = []
+  dimensions = parse(input)
+  result = []
 
-	until dimensions.size <= 0 
-		width = dimensions.shift()
-		height = dimensions.shift()
+  until dimensions.size <= 0 
+    width = dimensions.shift()
+    height = dimensions.shift()
 
-		result += resize(width, height)
-	end
+    result += resize(width, height)
+  end
 
-	result
+  result
 end
 
 def parse(str)
-	result = str[1..str.size].split(",")
-	result.map{|c| c.to_i }
+  result = str[1..str.size].split(",")
+  result.map{|c| c.to_i }
 end
 
 def resize(w,h)
-	orientation = orientation(w, h)
+  orientation = orientation(w, h)
 
-	if orientation == "landscape"
-		result = [200, get_height(w,h)]
-	else
-		result = [get_width(w,h), 200]
-	end
+  if orientation == "landscape"
+    result = [200, get_height(w,h)]
+  else
+    result = [get_width(w,h), 200]
+  end
 
-	result
+  result
 end
 
 def orientation(w,h)
-	w >= h ? "landscape" : "portrait"
+  w >= h ? "landscape" : "portrait"
 end
 
 def get_height(w,h)
-	y = 200*h / w
-	y
+  y = 200*h / w
+  y
 end
 
 def get_width(w,h)
-	x = 200*w / h
-	x
+  x = 200*w / h
+  x
 end
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -4,99 +4,99 @@ require "byebug"
 
 describe "#image_resizer" do
 
-	context "Analysis" do
+  context "Analysis" do
 
-		describe "Parse" do
-			it "parses a string representation of [width, height]" do
-				str = "[200,200]"
-				answer = [200,200]
+    describe "Parse" do
+      it "parses a string representation of [width, height]" do
+        str = "[200,200]"
+        answer = [200,200]
 
-				expect(parse(str)).to eq(answer)
-			end
+        expect(parse(str)).to eq(answer)
+      end
 
-			it "parses a string representation of [w,h,w,h,w,h]" do
-				str = "[200,200,400,300]"
-				answer = [200,200,400, 300]
+      it "parses a string representation of [w,h,w,h,w,h]" do
+        str = "[200,200,400,300]"
+        answer = [200,200,400, 300]
 
-				expect(parse(str)).to eq(answer)
-			end
-		end
+        expect(parse(str)).to eq(answer)
+      end
+    end
 
-		describe "Aspect Ratio" do
-			it "determines if the picture is landscape/square" do
-				width, height = 400,200
-				answer = "landscape"
+    describe "Aspect Ratio" do
+      it "determines if the picture is landscape/square" do
+        width, height = 400,200
+        answer = "landscape"
 
-				expect(orientation(width,height)).to eq(answer)
-			end
+        expect(orientation(width,height)).to eq(answer)
+      end
 
-			it "determines if the picture is portrait" do
-				width, height = 600,800
-				answer = "portrait"
+      it "determines if the picture is portrait" do
+        width, height = 600,800
+        answer = "portrait"
 
-				expect(orientation(width,height)).to eq(answer)
-			end
+        expect(orientation(width,height)).to eq(answer)
+      end
 
-			it "finds corresponding side of a square" do
-				width, height = 200, 200
-				answer = 200
+      it "finds corresponding side of a square" do
+        width, height = 200, 200
+        answer = 200
 
-				expect(get_height(width, height)).to eq(answer)
-			end
+        expect(get_height(width, height)).to eq(answer)
+      end
 
-			it "finds corresponding side of landscape" do 
-				width, height = 400, 200
-				answer = 100
+      it "finds corresponding side of landscape" do 
+        width, height = 400, 200
+        answer = 100
 
-				expect(get_height(width, height)).to eq(answer)
-			end
+        expect(get_height(width, height)).to eq(answer)
+      end
 
-			it "finds corresponding side of portrait" do 
-				width, height = 600, 800
-				answer = 150
+      it "finds corresponding side of portrait" do 
+        width, height = 600, 800
+        answer = 150
 
-				expect(get_width(width, height)).to eq(answer)
-			end
-		end
+        expect(get_width(width, height)).to eq(answer)
+      end
+    end
 
-		describe "Resize" do 
-			it "resizes a square image" do 
-				width, height = 200, 200
-				answer = [200,200]
+    describe "Resize" do 
+      it "resizes a square image" do 
+        width, height = 200, 200
+        answer = [200,200]
 
-				expect(resize(width,height)).to eq(answer)
-			end
+        expect(resize(width,height)).to eq(answer)
+      end
 
-			it "resizes a landscape image " do 
-				width, height = 400, 200
-				answer = [200,100]
+      it "resizes a landscape image " do 
+        width, height = 400, 200
+        answer = [200,100]
 
-				expect(resize(width,height)).to eq(answer)
-			end
+        expect(resize(width,height)).to eq(answer)
+      end
 
-			it "resizes a portrait image" do 
-				width, height = 600, 800
-				answer = [150, 200]
+      it "resizes a portrait image" do 
+        width, height = 600, 800
+        answer = [150, 200]
 
-				expect(resize(width,height)).to eq(answer)
-			end
-		end
+        expect(resize(width,height)).to eq(answer)
+      end
+    end
 
-		describe "Image Resizer" do
-			it "takes a pair of dimensions" do
-				input = "[400,200]"
-				answer = [200, 100]
+    describe "Image Resizer" do
+      it "takes a pair of dimensions" do
+        input = "[400,200]"
+        answer = [200, 100]
 
-				expect(image_resizer(input)).to eq(answer)
-			end
+        expect(image_resizer(input)).to eq(answer)
+      end
 
-			it "takes several pairs of dimensions" do
-				input = "[1256,1200, 600, 800, 200, 200, 400, 200, 800,1256]"
-				answer = [200, 191, 150, 200, 200, 200, 200, 100, 127, 200]
+      it "takes several pairs of dimensions" do
+        input = "[1256,1200, 600, 800, 200, 200, 400, 200, 800,1256]"
+        answer = [200, 191, 150, 200, 200, 200, 200, 100, 127, 200]
 
-				expect(image_resizer(input)).to eq(answer)
-			end
-		end
+        expect(image_resizer(input)).to eq(answer)
+      end
+    end
 
-	end
+  end
 end
